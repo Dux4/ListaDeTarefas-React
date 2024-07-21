48dadbe11d615e50840856143aabdffa5664d5b7
Dux <eduardopassos55551@gmail.com>
2024-07-21 01:45:40 -0300

    Front-end Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import ToDoList from "./ToDoList";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <ToDoList/>
+
   </React.StrictMode>,
 )
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,6 +1,31 @@
-function ToDoList(params) {
-    return(
+import React from "react";
+import './ToDoList.css'
+
+function ToDoList() {
+    return (
         <>
+        <div className="AddLista">
+            <h1>Lista de Tarefas</h1>
+
+            <form>
+                <input type="text" placeholder="Digite uma tarefa" />
+                <button className="add" type="submit" >Add</button>
+            </form>
+        </div>
+
+        <div className="ShowList">
+            <div className="item">
+                <span>Tarefa de exemplo</span>
+                <button className="del-btn">DELETE</button>
+            </div>
+
+            <div className="item completo" >
+                <span>Tarefa de exemplo</span>
+                <button className="del-btn">DELETE</button>
+            </div>
+            <button className="delAll-btn" >Deletar Todas</button>
+        </div>
+
         </>
     )
 }
